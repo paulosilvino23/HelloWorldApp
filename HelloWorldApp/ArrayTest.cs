a90335b37c12fcd32e75bfdf90cf5b5b7386dd29
Paulo S <paulosilvino23@hotmail.com>
2024-01-26 23:52:22 -0300

    Initial commit

---
+++ HelloWorldApp/ArrayTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+	public static class ArrayTest
+	{
+		public static void execute()
+		{
+            const String LINE_DIVISOR = "=====================================";
+
+            Student[] students = new Student[10];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                int sid = (i + 1 + 100);
+                students[i] = new Student(sid, "Student " + sid, "M");
+            }
+
+            Console.WriteLine(LINE_DIVISOR);
+
+            foreach (Student s in students)
+            {
+                Console.WriteLine(s.getDetails());
+            }
+
+            Console.WriteLine(LINE_DIVISOR);
+
+        }
+    }
+}
+
---
+++ HelloWorldApp/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+	public class Student
+	{
+		public Student(int sid, String name, String gender)
+		{
+			Sid = sid;
+			Name = name;
+			Gender = gender;
+		}
+
+		public int Sid { get; }
+
+		public String Name { get; set; }
+
+		public String Gender { get; set; }
+
+		public String getDetails()
+		{
+			return Sid + "\t" + Name + "\t" + Gender;
+		}
+    }
+}
+
